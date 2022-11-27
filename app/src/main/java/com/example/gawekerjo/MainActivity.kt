1750cd214576dda45213e13f6e2fda8b7143994b
Chrisanto <mxzeromxzero6@gmail.com>
2022-11-27 20:55:45 +0700

    infrastructure layout

--- app/src/main/java/com/example/gawekerjo/MainActivity.kt
+++ app/src/main/java/com/example/gawekerjo/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.gawekerjo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.gawekerjo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var b: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        b = ActivityMainBinding.inflate(layoutInflater)
+        val view = b.root
+        setContentView(view)
     }
 }
