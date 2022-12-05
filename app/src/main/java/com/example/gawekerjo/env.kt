4be4ab0462fa555d1c3be74caabc0cdc61c1b794
Chrisanto <mxzeromxzero6@gmail.com>
2022-12-05 16:50:14 +0700

    update env api url

--- app/src/main/java/com/example/gawekerjo/env.kt
+++ app/src/main/java/com/example/gawekerjo/env.kt
@@ -6,7 +6,7 @@ object env {
 
 //    const val API_URL ="http://127.0.0.1:8000/api/"
 //    const val API_URL = "http://192.168.1.6/api/"
-    const val API_URL = "https://2200-125-164-15-31.ap.ngrok.io/api/"
+    const val API_URL = "mhs.sib.stts.edu/k4nobar/GaweKerjo/GaweKerjo_API/public/api/"
     const val USER_ENDPOINT = "users"
 
     const val DB_CONNECTION="mysql"
