314bca089eec2acd34e88ed2e66275a36a60e357
uguivuigo9 <ckw28502@gmail.com>
2022-12-27 21:02:28 +0700

    Buat Layout chat

--- app/src/main/java/com/example/gawekerjo/view/ChatActivity.kt
+++ app/src/main/java/com/example/gawekerjo/view/ChatActivity.kt
@@ -3,10 +3,15 @@ package com.example.gawekerjo.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.gawekerjo.R
+import com.example.gawekerjo.databinding.ActivityChatBinding
 
 class ChatActivity : AppCompatActivity() {
+    lateinit var b:ActivityChatBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_chat)
+        b=ActivityChatBinding.inflate(layoutInflater)
+        val v=b.root
+        setContentView(v)
+
     }
 }
