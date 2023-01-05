4e7a3d438569d9a976dc24609cf8d7c2e17ba252
estergbriel <esther_g20@mhs.istts.ac.id>
2023-01-05 22:23:03 +0700

    UI
    UI

---
+++ app/src/main/java/com/example/gawekerjo/view/FriendListActivity.kt
@@ -0,0 +1,37 @@
+package com.example.gawekerjo.view
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import com.example.gawekerjo.R
+
+class FriendListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_friend_list)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menuaddfriend,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.menuaddfriend->{
+//                val intent = Intent(this,FavoriteActivity::class.java)
+//                intent.putParcelableArrayListExtra("arrBoy", arrBoyName)
+//                intent.putParcelableArrayListExtra("arrGirl", arrGirlName)
+//                addLauncher.launch(intent)
+                true
+            }
+            else->{
+                false
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+}
